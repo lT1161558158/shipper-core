9497fe3a6c2f95a7fa8e915c75fee77038967a9e
11615 <1161558158@qq.com>
2019-10-29 15:34:15 +0800

    增加注释

--- src/main/java/oh/my/shipper/core/api/Initialization.java
+++ src/main/java/oh/my/shipper/core/api/Initialization.java
@@ -3,7 +3,7 @@ package oh.my.shipper.core.api;
 /**
  * 初始化接口
  * 这个初始化接口实际上将在每次执行时调用
- * 因为filter和output实际上是可以运行时重载参数的
+ * 因为filter和output实际上是可以运行时重载参数的(实际上input也可以)
  * 也就是说若需要实现"仅一次""的初始化,则需要在init函数中实现"仅一次"的语义
  */
 public interface Initialization {
