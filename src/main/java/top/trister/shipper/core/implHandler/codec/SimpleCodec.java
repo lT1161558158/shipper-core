d8c6133ff042260c1e073ba45912ceb3c4556842
11615 <1161558158@qq.com>
2019-12-26 16:04:59 +0800

    移除SimpleCodec中的时间戳设置

--- src/main/java/top/trister/shipper/core/implHandler/codec/SimpleCodec.java
+++ src/main/java/top/trister/shipper/core/implHandler/codec/SimpleCodec.java
@@ -3,13 +3,10 @@ package top.trister.shipper.core.implHandler.codec;
 import lombok.Data;
 import top.trister.shipper.core.api.handler.codec.Codec;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import static top.trister.shipper.core.event.Event.MESSAGE;
-import static top.trister.shipper.core.event.Event.TIMESTAMP;
 
 /**
  * 添加一个时间filed发编码器
@@ -18,24 +15,24 @@ import static top.trister.shipper.core.event.Event.TIMESTAMP;
  */
 @Data
 public class SimpleCodec implements Codec<Object, Map> {
-    private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    private String format = DEFAULT_FORMAT;
-
-    private SimpleDateFormat builderFormat() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
-        try {
-            simpleDateFormat.applyPattern(format);
-        } catch (Exception ignore) {
-            simpleDateFormat.applyPattern(DEFAULT_FORMAT);
-            format = DEFAULT_FORMAT;
-        }
-        return simpleDateFormat;
-    }
+//    private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+//    private String format = DEFAULT_FORMAT;
+//
+//    private SimpleDateFormat builderFormat() {
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+//        try {
+//            simpleDateFormat.applyPattern(format);
+//        } catch (Exception ignore) {
+//            simpleDateFormat.applyPattern(DEFAULT_FORMAT);
+//            format = DEFAULT_FORMAT;
+//        }
+//        return simpleDateFormat;
+//    }
 
     @Override
     public Map codec(Object input) {
         Map<Object, Object> event = new HashMap<>();
-        event.put(TIMESTAMP, builderFormat().format(new Date()));
+//        event.put(TIMESTAMP, builderFormat().format(new Date()));
         event.put(MESSAGE, input);
         return event;
     }
